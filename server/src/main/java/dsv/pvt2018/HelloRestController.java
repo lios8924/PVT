61f7f98d0d3053995134b88d1b5b6a61f3974fa1
Oskarzito <oskar.kv@gmail.com>
2018-04-25 10:34:44 +0200

    Paket fixade

--- server/src/main/java/dsv/pvt2018/HelloRestController.java
+++ server/src/main/java/dsv/pvt2018/HelloRestController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+* Kan tas bort!
+*/
 @RestController
 @RequestMapping("/emil")
 public class HelloRestController {
