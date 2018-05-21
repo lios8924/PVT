aecc6ab0ee6e45cf6ed04f948a9f6f21794fa2f1
Victor Sago <VictorSago01@gmail.com>
2018-05-21 14:22:46 +0200

    Merged master into refactoring branch.

--- server/src/main/java/dsv/pvt2018/controllers/LampController.java
+++ server/src/main/java/dsv/pvt2018/controllers/LampController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import dsv.pvt2018.model.LampCapture;
 import dsv.pvt2018.model.Lamp;
 import dsv.pvt2018.services.LampService;
